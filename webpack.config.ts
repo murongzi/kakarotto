0898cfc1856379324ee1a5ed814efe8403861a9a
mazidong <mazidong@jk.cn>
2018-02-06 16:03:16 +0800

    配置webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,9 +2,11 @@ import * as webpack from 'webpack';
 import * as path from 'path';
 
 const config: webpack.Configuration = {
-    entry: './src/index',
+    entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js'
     }
 }
+
+export default config;
