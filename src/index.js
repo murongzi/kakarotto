b4ad11d581f8c96fa4e9cd5e08e07c4bba24b64f
mazidong <970517184@qq.com>
2026-04-29 16:39:56 +0800

    添加正则表达式共sonar测试

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ console.log('reg', reg1.test('aa@qq.com'))
 
 
 
-const reg2 = /^[a-zA-Z0-9-_]{1,64}@[a-zA-Z0-9](?:[a-zA-Z0-9-_]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-_]{0,253}[a-zA-Z0-9])?)*\.[a-zA-Z]{2,6}$/;
+const reg2 = /^[a-zA-Z0-9-_]{1,64}@[a-zA-Z0-9](?:[a-zA-Z0-9-_]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-_]{0,253}[a-zA-Z0-9])?)*\.[a-zA-Z]{2,3}$/
 
 console.log('reg', reg2.test('aa@qq.com'))
+
+const func = ''.match(/=>\s*\{(.*)\}/s)[1] || ''
+const fn = new Function('data', func) // NOSONAR
+const a = fn(result.data)
+console.log('a', a)
