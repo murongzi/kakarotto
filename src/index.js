03d3526a138a43b6fac6a2fcc15261e57b61ebd8
mazidong <970517184@qq.com>
2026-04-29 15:35:38 +0800

    添加正则表达式共sonar测试

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './app/index.scss';
+
+const reg1 = /^([a-zA-Z0-9-_]+[\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9-_]+[\.]?)*[a-zA-Z0-9]+.[a-zA-Z]{2,3}$/;
+
+console.log('reg', reg1.test('aa@qq.com'))
+
+
+
+const reg2 = /^([a-zA-Z0-9\-_]+?[\.]?)*?[a-zA-Z0-9]+?@([a-zA-Z0-9\-_]+?[\.]?)*?[a-zA-Z0-9]+.?[a-zA-Z]{2,3}$/;
+
+console.log('reg', reg2.test('aa@qq.com'))
