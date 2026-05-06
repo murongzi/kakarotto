356c242cd57cc3247ea4df62a8e7d78662a4f575
mazidong <970517184@qq.com>
2026-05-06 11:08:58 +0800

    fix:sonar去掉多余代码

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './app/index.scss';
 
-const reg1 = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+/g;;
+const reg1 = /[a-zA-Z0-9._%+-]{1,64}@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*\.[a-zA-Z]{2,10}/g;
 
 console.log('reg', reg1.test('aa@qq.com'))
 
